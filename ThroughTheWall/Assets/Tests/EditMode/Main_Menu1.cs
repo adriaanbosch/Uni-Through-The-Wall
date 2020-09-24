41cf55fbbc5ab6cfbe8b3fc84d2df79524f4546d
Dhanan <64773747+Dhanan24@users.noreply.github.com>
2020-09-24 19:44:17 +1200

    unit test added

---
+++ ThroughTheWall/Assets/Tests/EditMode/Main_Menu1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadArena()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
