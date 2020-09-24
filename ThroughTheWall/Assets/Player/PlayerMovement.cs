fff59b715d99e9f53a3740fb3fbef7db628d4619
adriaanbosch36 <65434881+adriaanbosch36@users.noreply.github.com>
2020-09-24 19:04:52 +1200

    TDD forPlayer can lose game, and code written
    TDD code has been written, testing that the player can "die" and lose the game

--- ThroughTheWall/Assets/Player/PlayerMovement.cs
+++ ThroughTheWall/Assets/Player/PlayerMovement.cs
@@ -11,8 +11,8 @@ public class PlayerMovement : MonoBehaviour
 	float gravity = 12f;
 	float rotSpeed = 100f;
 	float rot = 0f;
-
-	Vector3 moveDir = Vector3.zero;
+    float pickupRange = 2;
+    Vector3 moveDir = Vector3.zero;
 
 	CharacterController controller;
 	Animator anim;
@@ -28,34 +28,76 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		if (controller.isGrounded)
-		{
-			moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDir = Vector3.ClampMagnitude(moveDir, 1);
-			moveDir = transform.TransformDirection(moveDir);
-			moveDir *= speed;
+        Movement();
+	}
+    void Movement()
+    {
+        if (controller.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDir = Vector3.ClampMagnitude(moveDir, 1);
+            moveDir = transform.TransformDirection(moveDir);
+            moveDir *= speed;
 
-			if (Input.GetButtonDown("Vertical"))
-			{
-				anim.SetInteger("condition", 1);
-			}
-
-			if (Input.GetButtonDown ("Jump"))
+            if (Input.GetButtonDown("Vertical"))
             {
-				anim.SetInteger("condition", 2);
-				moveDir.y = jumpForce;
+                anim.SetInteger("condition", 1);
             }
 
-			if (Input.GetButtonUp("Vertical") || Input.GetButtonUp("Jump"))
-			{
-				anim.SetInteger("condition", 0);
-			}
-		}
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetInteger("condition", 2);
+                moveDir.y = jumpForce;
+            }
 
-		rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
-		transform.eulerAngles = new Vector3(0, rot, 0);
+            if (Input.GetButtonUp("Vertical") || Input.GetButtonUp("Jump"))
+            {
+                anim.SetInteger("condition", 0);
+            }
+        }
 
-		moveDir.y -= gravity * Time.deltaTime;
-		controller.Move(moveDir * Time.deltaTime);
-	}
+        rot += Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+        transform.eulerAngles = new Vector3(0, rot, 0);
+
+        moveDir.y -= gravity * Time.deltaTime;
+        controller.Move(moveDir * Time.deltaTime);
+    }
+    
+    void ArmControl()
+    {
+        if (Input.GetAxis("Mouse Y") > 0)
+        {
+            print("Mouse moved up");
+            //something something animation
+        }
+        if (Input.GetAxis("Mouse Y") < 0)
+        {
+            print("Mouse moved down");
+            //something something animation	
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed primary button.");
+            GrabObject();
+        }
+
+
+    }
+    GameObject GetObjectMouseHover()// raycasts to find what opject is infront of the player
+    {
+        Vector3 position = gameObject.transform.position;
+        RaycastHit raycastHit;
+        Vector3 target = position + gameObject.transform.forward * pickupRange;
+        if (Physics.Linecast(position, target, out raycastHit))
+        {
+            return raycastHit.collider.gameObject;
+        }
+        return null;
+    }
+    void GrabObject() // uses the fixed joint attached to the character to connect / grab objects // not fully working yet
+    {
+        GameObject objects = GetObjectMouseHover();
+        gameObject.GetComponent<FixedJoint>().connectedBody = objects.GetComponent<Rigidbody>();
+        Debug.Log("fixed joint should be connected");
+    }
 }
