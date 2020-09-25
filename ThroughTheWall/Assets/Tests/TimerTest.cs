3968e5c94c866e01334b4ac57a83a5ad5e5f76d4
fsf4965 <67015888+fsf4965@users.noreply.github.com>
2020-09-25 12:23:14 +1200

    Timer

---
+++ ThroughTheWall/Assets/Tests/TimerTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TimerTest
+    {
+        [Test]
+        public void SecondsToMinutesTest()
+        {
+            float secondsCount = 0f;
+            int minuteCount = 0;
+
+            while (secondsCount < 60)
+            {
+                ++secondsCount;
+                if (secondsCount >= 60)
+                {
+                    minuteCount++;
+                }
+            }
+
+            Assert.AreEqual(1, minuteCount);
+        }
+
+        [Test]
+        public void MinutesToHoursTest()
+        {
+            int minuteCount = 0;
+            int hourCount = 0;
+
+            while (minuteCount < 60)
+            {
+                ++minuteCount;
+                if (minuteCount >= 60)
+                {
+                    hourCount++;
+                }
+            }
+
+            Assert.AreEqual(1, hourCount);
+        }
+    }
+}
